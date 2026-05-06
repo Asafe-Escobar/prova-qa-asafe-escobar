4aa302de250d238c1584d1d413f5ee6fdda6a3b0
asafe-escobar <jeanderson.escobar@somosicev.com>
2026-05-06 14:48:57 -0300

    feat(api): estrutura inicial do projeto com configuracao do pytest

---
+++ automacao-api/config.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://petstore.swagger.io/v2"
+
+TIMEOUT = 10
+
+DEFAULT_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
---
+++ automacao-api/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from utils.api_client import ApiClient
+
+
+@pytest.fixture(scope="session")
+def api():
+    return ApiClient()

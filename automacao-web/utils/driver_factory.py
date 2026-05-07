dcb1d64036930362210194d8f230e7d4b13d5727
asafe-escobar <jeanderson.escobar@somosicev.com>
2026-05-07 19:13:27 -0300

    fix: desativa deteccao de senha vazada do chrome

--- automacao-web/utils/driver_factory.py
+++ automacao-web/utils/driver_factory.py
@@ -7,12 +7,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 def criar_driver(headless=False):
     options = Options()
 
-    # flags necessarias pra rodar no ubuntu do github actions
     if headless:
+        # flags necessarias pra rodar no ubuntu do github actions
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
+    else:
+        # desativa popups do gerenciador de senhas na execucao local
+        prefs = {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False,
+            "profile.password_manager_leak_detection": False,
+        }
+        options.add_experimental_option("prefs", prefs)
 
     options.add_argument("--start-maximized")
     options.add_argument("--disable-notifications")

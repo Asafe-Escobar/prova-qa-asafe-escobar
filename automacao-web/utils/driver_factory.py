8a742248aebdedfe1714cc386e2dede2f74109db
asafe-escobar <jeanderson.escobar@somosicev.com>
2026-05-06 21:12:32 -0300

    fix: reverte prefs do chrome que quebravam o ci

--- automacao-web/utils/driver_factory.py
+++ automacao-web/utils/driver_factory.py
@@ -12,20 +12,11 @@ def criar_driver(headless=False):
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
 
     options.add_argument("--start-maximized")
     options.add_argument("--disable-notifications")
-    options.add_argument("--disable-popup-blocking")
     options.add_argument("--disable-save-password-bubble")
-
-    prefs = {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-        "autofill.profile_enabled": False,
-    }
-    options.add_experimental_option("prefs", prefs)
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
